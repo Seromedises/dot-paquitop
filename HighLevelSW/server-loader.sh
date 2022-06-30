a9044eb9f2f30ebdde6ca34e1cd7be076ae5cb95
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-06-30 17:27:04 +0200

    two machine integration

--- HighLevelSW/server-loader.sh
+++ HighLevelSW/server-loader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-source ~/dot_paquitiop/HighLevelSW/devel/setup.bash
+source ~/dot-paquitiop/HighLevelSW/devel/setup.bash
 
 export ROS_IP=172.21.15.100
 export ROS_MASTER_URI=http://172.21.15.100:11311
