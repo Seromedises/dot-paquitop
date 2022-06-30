0ff2f51cd6421f779e0039e8e5a9560893c09bfc
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-06-30 16:02:28 +0200

    two machine file .sh configuring added

---
+++ HighLevelSW/paquitop-loader.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/ros/melodic/setup.bash
+source ~/dot_paquitiop/HighLevelSW/devel/setup.bash
+
+export ROS_IP=172.21.15.99
+export ROS_MASTER_URI=http://172.21.15.100:11311
+
+exec "$@"
