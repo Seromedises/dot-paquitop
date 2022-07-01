ef79080de6b3e79dfd2d5f0212fcf3a399401a80
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-07-01 17:07:33 +0200

    gui interface interaction with robot and aruco markers

--- HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
+++ HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
@@ -8,18 +8,21 @@ from turtle import color
 
 # Per aprire l'interfaccia a schermo intero:
 from kivy.core.window import Window
-
 import numpy as np
 import cv2
 import sys
 import pyrealsense2 as rs
-
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.image import Image 
 from kivy.uix.video import Video 
 from kivy.config import Config
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout 
+from kivymd.uix.menu import MDDropdownMenu 
+import rospy
+from std_msgs.msg import Empty, Bool
 
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
@@ -29,11 +32,7 @@ Config.set('graphics', 'fullscreen', 1)
 Config.set('graphics', 'window_state', 'maximized')
 Config.write()
 
-from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout 
-from kivymd.uix.menu import MDDropdownMenu 
 
-import rospy
 
 class DOT_PAQUITOP_GUI(MDApp):
 
@@ -63,8 +62,6 @@ class DOT_PAQUITOP_GUI(MDApp):
         arucoDict = cv2.aruco.Dictionary_get(default)
         arucoParams = cv2.aruco.DetectorParameters_create()
 
-
-
         # Get frameset of color and depth
         frames = self.pipeline.wait_for_frames()
         aligned_frames = self.align.process(frames)
@@ -77,30 +74,37 @@ class DOT_PAQUITOP_GUI(MDApp):
 
         if len(corners) > 0:
             # flatten the ArUco IDs list
-                ids = ids.flatten()
+            ids = ids.flatten()
 
-                for (markerCorner, markerID) in zip(corners, ids):
-                    # extract the marker corners (which are always returned in
-                    # top-left, top-right, bottom-right, and bottom-left order)
-                    corners = markerCorner.reshape((4, 2))
-                    (topLeft, topRight, bottomRight, bottomLeft) = corners
-                    # convert each of the (x, y)-coordinate pairs to integers
-                    topRight = (int(topRight[0]), int(topRight[1]))
-                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-                    topLeft = (int(topLeft[0]), int(topLeft[1]))
+            for (markerCorner, markerID) in zip(corners, ids):
+                # extract the marker corners (which are always returned in
+                # top-left, top-right, bottom-right, and bottom-left order)
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-                    cv2.line(images, topLeft, topRight, (0, 255, 0), 2)
-                    cv2.line(images, topRight, bottomRight, (0, 255, 0), 2)
-                    cv2.line(images, bottomRight, bottomLeft, (0, 255, 0), 2)
-                    cv2.line(images, bottomLeft, topLeft, (0, 255, 0), 2)
-                    # compute and draw the center (x, y)-coordinates of the ArUco
-                    # marker
-                    cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-                    cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                    cv2.circle(images, (cX, cY), 4, (0, 0, 255), -1)
-                    # draw the ArUco marker ID on the image
-                    cv2.putText(images, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.line(images, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(images, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(images, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(images, bottomLeft, topLeft, (0, 255, 0), 2)
+                # compute and draw the center (x, y)-coordinates of the ArUco
+                # marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(images, (cX, cY), 4, (0, 0, 255), -1)
+                # draw the ArUco marker ID on the image
+                cv2.putText(images, str(markerID),(topLeft[0] - 15, topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        if not robotic_arm_up:
+                extract = rospy.Publisher("/extract_tablet", Bool, queue_size=1)
+                extract_msg = Bool()
+                extract_msg.data = True
+                extract.publish(extract_msg)
+                robotic_arm_up = True
 
         frame = cv2.resize(images, None, fx=1.0, fy=1.0, interpolation=cv2.INTER_AREA)
 
@@ -117,8 +121,18 @@ class DOT_PAQUITOP_GUI(MDApp):
     def goON(self, *args):
         self.layout.ids.identification.md_bg_color = (200/255,200/255,200/255,1)
         self.layout.ids.identification.text = "Waiting for identifier"
+        # Tablet store
+        retrain = rospy.Publisher("/retrain_tablet", Bool, queue_size=1)
+        retrain_msg = Bool()
+        retrain_msg.data = True
+        retrain.publish(retrain_msg)
+        # Update status
+        robotic_arm_up = False
+        
 
 if __name__ == '__main__':
+    global robotic_arm_up
+    robotic_arm_up = False
     rospy.init_node('paquitop_gui')
     DOT_PAQUITOP_GUI().run()
     
