ab1aabae17be5dfa3bdff4f7796185422c50632c
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-07-05 10:19:47 +0200

    bugs fix

--- HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
+++ HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
@@ -49,6 +49,7 @@ class DOT_PAQUITOP_GUI(MDApp):
         align_to = rs.stream.color
         self.align = rs.align(align_to)
         #initialize topic extract table
+        count = 0
         while count < 3:
                 count = count +1
                 tab_ext = rospy.Publisher("/extract_tablet", Bool, queue_size=1)
