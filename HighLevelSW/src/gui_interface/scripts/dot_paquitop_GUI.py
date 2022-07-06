505ae2ca4ba2b9e9b38082a0e39a8cba76575151
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-07-06 12:02:44 +0200

    debugging

--- HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
+++ HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
@@ -123,11 +123,11 @@ class DOT_PAQUITOP_GUI(MDApp):
                 self.identificationOK()
             
             
-            if (self.markerID == 0 or self.markerID == 1) and not self.seat1:
+            if (self.markerID == 0 or self.markerID == 1) and not self.seat1 and self.arm_position:
                 self.seat1 = True
                 self.goUP()
 
-            if (self.markerID == 2 or self.markerID == 3) and not self.seat2:
+            if (self.markerID == 2 or self.markerID == 3) and not self.seat2 and self.arm_position:
                 self.seat2 = True
                 self.goUP()
             
@@ -198,6 +198,7 @@ class DOT_PAQUITOP_GUI(MDApp):
     
 def is_in_movement(movePAQUITOP):
     global PAQUITOP_STOP
+    print(movePAQUITOP)
     
     if movePAQUITOP.linear.x < 0.05 and movePAQUITOP.linear.y < 0.05 and movePAQUITOP.angular.z <0.1:
         PAQUITOP_STOP = True
