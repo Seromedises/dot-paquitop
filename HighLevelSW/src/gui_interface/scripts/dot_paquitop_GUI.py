3c7e735e56e17aa663383c838d50376aa2125caa
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-07-13 11:45:05 +0200

    bugs fix

--- HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
+++ HighLevelSW/src/gui_interface/scripts/dot_paquitop_GUI.py
@@ -15,8 +15,8 @@ from kivy.uix.image import Image
 from kivy.uix.video import Video 
 from kivy.config import Config
 from kivymd.app import MDApp
-#from kivymd.uix.boxlayout import MDBoxLayout 
-#from kivymd.uix.menu import MDDropdownMenu 
+from kivymd.uix.boxlayout import MDBoxLayout 
+from kivymd.uix.menu import MDDropdownMenu 
 import rospy
 from std_msgs.msg import Empty, Bool, Int64, String
 import rospkg
