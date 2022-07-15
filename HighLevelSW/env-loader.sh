e9926194efa69dda29391316972b343e5136c745
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2022-07-15 15:21:01 +0200

    ip adress

--- HighLevelSW/env-loader.sh
+++ HighLevelSW/env-loader.sh
@@ -2,9 +2,10 @@
 
 source /opt/ros/melodic/setup.bash
 source ~/dot-paquitop/HighLevelSW/devel/setup.bash
+my_ip=$(hostname -I | cut -d " " -f 1)
 if [ "$my_ip" == 172.21.15.99 ]; then
 	export ROS_IP=$my_ip
-	export ROS_MASTER_URI=http://172.21.15.99:11311
+	export ROS_MASTER_URI=http://172.21.15.100:11311
 fi
 if [ "$my_ip" == 192.168.8.229 ]; then
 	export ROS_IP=$my_ip
--- HighLevelSW/server-loader.sh
+++ HighLevelSW/server-loader.sh
@@ -2,7 +2,15 @@
 
 source /opt/ros/melodic/setup.bash
 source ~/dot-paquitiop/HighLevelSW/devel/setup.bash
-export ROS_IP=172.21.15.100
-export ROS_MASTER_URI=http://172.21.15.100:11311
+# export ROS_IP=172.21.15.100
+# export ROS_MASTER_URI=http://172.21.15.100:11311
+if [ "$my_ip" == 172.21.15.100 ]; then
+	export ROS_IP=$my_ip
+	export ROS_MASTER_URI=$my_ip:11311
+fi
+if [ "$my_ip" == 192.168.8.229 ]; then
+	export ROS_IP=$my_ip
+	export ROS_MASTER_URI=$my_ip:11311	
+fi
 
 exec "$@"
