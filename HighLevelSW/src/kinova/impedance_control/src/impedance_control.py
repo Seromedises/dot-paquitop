69c134506a5cd19230161978fa37690379df0b74
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2023-01-13 14:55:02 +0100

    impedance controlo updates

--- HighLevelSW/src/kinova/impedance_control/src/impedance_control.py
+++ HighLevelSW/src/kinova/impedance_control/src/impedance_control.py
@@ -3,13 +3,15 @@
 import numpy as np
 import scipy.fftpack as fftpack
 import matplotlib.pyplot as plt
+import rospy
+from std_msgs.msg import Float64
 
 def filtering_fct(raw_signal):
   dct = fftpack.dct(raw_signal, norm="ortho")
   dct[8:] = 0
   return fftpack.idct(dct, norm="ortho")
 
-def plot_fct(raw,filterd,time):
+def plot_fct_raw(raw,filterd,time):
 
   i = 0
   span = 40
@@ -34,3 +36,45 @@ def plot_fct(raw,filterd,time):
       else:
           plt.show()
 
+def plot_fdt(filterd):
+
+  
+  plt.plot(range(len(filterd)-1), filterd)
+  plt.draw()
+  plt.pause(0.00001)
+  plt.clf()
+  """else:
+      plt.show()"""
+
+
+def lenght_ctrl(variable, span = 100):
+  
+  if len(variable) > span:
+    variable = variable[-span]
+  
+  return variable
+
+
+def main():
+  rospy.init_node("impedance_control")
+  Fx, Fy, Fz, Tx, Ty, Tz = []
+
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_force_x", Float64, Fx.append())
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_force_y", Float64, Fy.append())
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_force_z", Float64, Fz.append())
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_torque_x", Float64, Tx.append())
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_torque_y", Float64, Ty.append())
+  rospy.Subscriber("/my_gen3_lite/base_feedback/base/tool_external_wrench_torque_z", Float64, Tz.append())
+  
+  while not rospy.is_shutdown():
+    Fx = filtering_fct(lenght_ctrl(Fx))
+    Fy = filtering_fct(lenght_ctrl(Fy))
+    Fz = filtering_fct(lenght_ctrl(Fz))
+    Tx = filtering_fct(lenght_ctrl(Tx))
+    Ty = filtering_fct(lenght_ctrl(Ty))
+    Tz = filtering_fct(lenght_ctrl(Tz))
+
+
+
+if __name__ == "__main__":
+  main()
