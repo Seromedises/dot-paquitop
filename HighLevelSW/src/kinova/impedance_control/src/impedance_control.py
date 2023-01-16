9fc7f77b38af6d68d9f17a891f21ee1ac8ae5eba
Lorenzo Baglieri <lorenzobaglieri@gmail.com>
2023-01-16 16:33:31 +0100

    impedance control updates

--- HighLevelSW/src/kinova/impedance_control/src/impedance_control.py
+++ HighLevelSW/src/kinova/impedance_control/src/impedance_control.py
@@ -6,65 +6,85 @@ import matplotlib.pyplot as plt
 import rospy
 from kortex_driver.msg import BaseCyclic_Feedback
 
-def filtering_fct(raw_signal):
+# Constant for translate input force in velocity output
+OUT_max = 1 # m/s
+OUT_lim = 0.1 # m/s
+
+IN_lim = 1 # N
+IN_max = 4 # N
+
+b1 = (OUT_max-OUT_lim)/(IN_max-IN_lim)
+b0 = OUT_max - b1*IN_max
+a3 = 2*(5*OUT_max - 5*b1*IN_max + 3*b1*IN_lim)/(IN_lim**3)
+a4 = -(15*OUT_max - 15*b1*IN_max + 8*b1*IN_lim)/(IN_lim**4)
+a5 = 3*(2*OUT_max - 2*b1*IN_max + b1*IN_lim)/(IN_lim**5)
+
+def plot_fct(filterd, velocity, title):
   
-  """span = 150
-  raw_signal = raw_signal[-span:]"""
-
-  dct = fftpack.dct(raw_signal, norm="ortho")
-  dct[8:] = 0
-  return fftpack.idct(dct, norm="ortho")
-
-"""def plot_fct_raw(raw,filterd,time):
-
-  i = 0
-  span = 40
-  rawi = raw[0]
-
-  while i <= len(raw):
-      i = i + 1
-
-      if i > span:
-          rawi = raw[i-span:i]
-          xi = time[i-span:i]
-      else:
-          rawi = raw[0:i]
-          xi = time[0:i]
-
-      plt.plot(xi, rawi, ".")
-      plt.plot(xi, filterd)
-      plt.draw()
-      plt.pause(0.00001)
-      if i != len(raw) + 1:
-          plt.clf()
-      else:
-          plt.show()"""
-
-def plot_fct(filterd,title):
-
-  
+  plt.subplot(2, 1, 1)
   plt.plot(filterd)
   plt.title(title)
   plt.xlabel("numbers of acquisitions")
   plt.ylabel("Force [N]")
   plt.grid()
-  plt.ylim(ymax = 5, ymin = -5)
+  plt.ylim(ymax = 1/2 + IN_max, ymin = -(1/2 + IN_max))
+
+  plt.subplot(2, 1, 2)
+  plt.plot(velocity)
+  plt.xlabel("numbers of acquisitions")
+  plt.ylabel("velocity [m/s]")
+  plt.grid()
+  plt.ylim(ymax = 1/2 + OUT_max, ymin = -(1/2 + OUT_max))
+
   plt.draw()
-  plt.pause(0.00001)
+  plt.pause(0.0001)
   plt.clf()
-  
-def lenght_ctrl(variable, span = 100):
-  
+
+def length_control(variable,span=100):
+
   if len(variable) > span:
     variable = variable[-span:]
   
   return variable
 
+def variable_control(variable, offset=0, span=100):
+
+  variable = length_control(variable,span)
+
+  #if len(variable) < span:
+  #  offset = sum(variable)/len(variable)
+
+  dct = fftpack.dct(variable, norm="ortho")
+  dct[8:] = 0
+  filtred = fftpack.idct(dct, norm="ortho")
+
+  for i in range(len(filtred)):
+    filtred[i] = filtred[i]- offset 
+
+  return filtred, variable, offset
+
+def force_to_velocity(IN):
+
+  velocity = 0
+
+  if IN <= -IN_lim:
+    velocity = b1 * IN - b0
+  elif IN > -IN_lim and IN <= 0:
+    IN = -IN
+    velocity = -(a5*(IN**5) + a4*(IN**4) + a3*(IN**3))
+  elif IN > 0 and IN <= IN_lim:
+    velocity = a5*(IN**5) + a4*(IN**4) + a3*(IN**3)
+  elif IN > IN_lim:
+    velocity = b1 * IN + b0
+
+  return velocity
 
 def main():
   rospy.init_node("impedance_control")
   Fx, Fy, Fz, Tx, Ty, Tz = [], [], [], [], [], []
-    
+  Fx_ofs, Fy_ofs, Fz_ofs, Tx_ofs, Ty_ofs, Tz_ofs = 0, 0, 0, 0, 0, 0
+  vx = []
+  
   while not rospy.is_shutdown():
     data = rospy.wait_for_message("/my_gen3_lite/base_feedback",BaseCyclic_Feedback)
 
@@ -75,21 +95,19 @@ def main():
     Ty.append(data.base.tool_external_wrench_torque_y)
     Tz.append(data.base.tool_external_wrench_torque_z)
 
-    Fx = lenght_ctrl(Fx,span=250)
-    Fy = lenght_ctrl(Fx,span=250)
-    Fz = lenght_ctrl(Fx,span=250)
-    Tx = lenght_ctrl(Tx,span=250)
-    Ty = lenght_ctrl(Ty,span=250)
-    Tz = lenght_ctrl(Tz,span=250)
 
-    Fx_mean = filtering_fct(Fx)
-    Fy_mean = filtering_fct(Fy)
-    Fz_mean = filtering_fct(Fz)
-    Tx_mean = filtering_fct(Tx)
-    Ty_mean = filtering_fct(Ty)
-    Tz_mean = filtering_fct(Tz)
 
-    plot_fct(Fx_mean ,title="$F_x$ and $F_y$ mean value")
+    Fx_mean, Fx, Fx_ofs = variable_control(Fx, Fx_ofs, span=250)
+    Fy_mean, Fy, Fy_ofs = variable_control(Fy, Fy_ofs, span=250)
+    Fz_mean, Fz, Fz_ofs = variable_control(Fz, Fz_ofs, span=250)
+    Tx_mean, Tx, Tx_ofs = variable_control(Tx, Tx_ofs, span=250)
+    Ty_mean, Ty, Ty_ofs = variable_control(Ty, Ty_ofs, span=250)
+    Tz_mean, Tz, Tz_ofs = variable_control(Tz, Tz_ofs, span=250)
+
+    vx.append(force_to_velocity(Fx_mean[-1]))
+    vx = length_control(vx, span = 250)
+
+    plot_fct(Fx_mean, vx , title="$F_x$ mean value and $v_x$ output value")
     
 
 if __name__ == "__main__":
